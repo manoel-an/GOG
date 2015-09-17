21135a0ae1d0b9756181738a49e83d94c56c5769
Guthierrez <guthi.inf@gmail.com>
2015-09-17 10:39:35 -0300

    Ajuste troca de mensagens entre ouvidor e manifestante

--- src/main/java/br/com/xti/ouvidoria/controller/MBListarManifestacoes.java
+++ src/main/java/br/com/xti/ouvidoria/controller/MBListarManifestacoes.java
@@ -199,7 +199,7 @@ public class MBListarManifestacoes implements Serializable {
         filtro.setMetodoBusca("and");
         // -- ADMIN ou OUVIDOR
         if(securityService.isAdministrador() || securityService.isOuvidor()) {
-	        filtro.setEncStatus(StatusEncaminhamentoEnum.ENCAMINHADA.getId());
+        	filtro.setEncStatus(StatusEncaminhamentoEnum.ENCAMINHADA.getId());
 	        filtro.addManIdStatus(StatusManifestacaoEnum.EM_ANDAMENTO.getId());
 //	        filtro.addManIdStatus(StatusManifestacaoEnum.SOLICITADA_INFORMACAO.getId());
         } else if(securityService.isInterlocutor()) {
